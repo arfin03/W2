fc3aefbae359c631ec91a4fc8ed9ac2ef708dd7f
Un-bots <161623523+Un-bots@users.noreply.github.com>
2024-03-29 05:22:05 +0530

    Update ping.py

--- shivu/modules/ping.py
+++ shivu/modules/ping.py
@@ -7,12 +7,12 @@ from shivu import application, sudo_users
 
 async def ping(update: Update, context: CallbackContext) -> None:
     if str(update.effective_user.id) not in sudo_users:
-        update.message.reply_text("Noiii.. its Sudo user's Command..")
+        update.message.reply_text("Nᴏɪɪɪ.. ɪᴛs Sᴜᴅᴏ ᴜsᴇʀ's Cᴏᴍᴍᴀɴᴅ🥱")
         return
     start_time = time.time()
     message = await update.message.reply_text('Pong!')
     end_time = time.time()
     elapsed_time = round((end_time - start_time) * 1000, 3)
-    await message.edit_text(f'Pong! {elapsed_time}ms')
+    await message.edit_text(f'🏓ᴘᴏɴɢ! {elapsed_time}ms ✨ \n I'ᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ👻')
 
 application.add_handler(CommandHandler("ping", ping))
