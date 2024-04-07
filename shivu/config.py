03ed8ed53455ce0c6f4498fc6602ecf5d1cf1951
Harsha <158590882+Harsh-un@users.noreply.github.com>
2024-04-07 22:08:12 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,7 +5,7 @@ class Config(object):
     OWNER_ID = "6253265083"
     sudo_users = "6253265083", "5382284735", "5297949798", "6338745050", "1466012993", "5348552949"
     GROUP_ID = "-1002096627846"
-    TOKEN = "7088122814:AAEjGpQC9tcIcyYthbeRJKvAfwZhgJNcA1Y"
+    TOKEN = "6992015243:AAEw0sazm80dQLcgYUeufjwtaagp5HqqYZM"
     mongo_url = "mongodb+srv://BrandedSupportGroup:BRANDED_WORLD@cluster0.v4odcq9.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/7565b6a25a480d4dad36e.jpg", "https://telegra.ph/file/f7da678981007299d5a0e.jpg"]
     SUPPORT_CHAT = "WAIFU_GRABBERS"
