7ff6ed51e324a42042379d75b607f32acfc517bd
arfin03 <156934678+arfin03@users.noreply.github.com>
2024-05-07 18:42:44 +0700

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,8 +2,8 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6253265083"
-    sudo_users = "6253265083", "5382284735", "5297949798", "6338745050", "1466012993", "5348552949"
+    OWNER_ID = "6995317382"
+    sudo_users = "6995317382", "5382284735", "5297949798", "6338745050", "1466012993", "5348552949"
     GROUP_ID = "-1002096627846"
     TOKEN = "6992015243:AAEw0sazm80dQLcgYUeufjwtaagp5HqqYZM"
     mongo_url = "mongodb+srv://BrandedSupportGroup:BRANDED_WORLD@cluster0.v4odcq9.mongodb.net/?retryWrites=true&w=majority"
